793492d348ca488a3071534248d2d1547161083e
eladiocm <eladio970826@gmail.com>
2023-07-15 18:48:20 -0400

    feat: :sparkles: initial configuration for the server

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import db from "./config/mongo";
+import { router } from "./routes";
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+db().then(() => console.log("Connection Ready")).catch((err) => console.log('Error', err));
+app.listen(PORT, () => console.log(`Listo por el puerto ${PORT}`));
